f450b1b54bd9fe9e89ef475bae1b69d620ca9cd3
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-30 11:42:16 +0200

    refactor VehiculeController to simplify endpoint mappings and add VehiculeRequests.http for API request examples

--- src/main/java/com/humanbooster/controller/VehiculeController.java
+++ src/main/java/com/humanbooster/controller/VehiculeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/vehicules")
 public class VehiculeController {
 
     private final VehiculeService vehiculeService;
@@ -18,24 +19,24 @@ public class VehiculeController {
         this.vehiculeService = vehiculeService;
     }
 
-    @GetMapping("/vehicules")
+    @GetMapping
     public List<Vehicule> getAllVehicules() {
         return vehiculeService.getAllVehicules();
     }
 
-    @GetMapping("/vehicules/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Vehicule> getVehiculeById(@PathVariable int id) {
         return vehiculeService.getVehiculeById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/vehicules")
+    @PostMapping
     public void saveVehicule(@RequestBody Vehicule vehicule) {
         vehiculeService.saveVehicule(vehicule);
     }
 
-    @PutMapping("/vehicules/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Vehicule> updateVehicule(@PathVariable int id, @RequestBody Vehicule vehicule) {
         return vehiculeService.getVehiculeById(id)
                 .map(existingVehicule -> vehiculeService.updateVehicule(vehicule)
@@ -44,7 +45,7 @@ public class VehiculeController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/vehicules/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Vehicule> deleteVehiculeById(@PathVariable int id) {
         return vehiculeService.deleteVehiculeById(id)
                 .map(vehicule -> ResponseEntity.ok().body(vehicule))
