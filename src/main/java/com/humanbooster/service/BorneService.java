55d332f5e10a36cc2d0bb1871283a5d3a2fa54b0
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-30 11:24:39 +0200

    refactor BorneService to enhance saveBorne method with address and user creation logic

--- src/main/java/com/humanbooster/service/BorneService.java
+++ src/main/java/com/humanbooster/service/BorneService.java
@@ -1,7 +1,13 @@
 package com.humanbooster.service;
 
+import com.humanbooster.model.Adresse;
 import com.humanbooster.model.Borne;
+import com.humanbooster.model.Lieu;
+import com.humanbooster.model.Utilisateur;
+import com.humanbooster.repository.AdresseRepository;
 import com.humanbooster.repository.BorneRepository;
+import com.humanbooster.repository.LieuRepository;
+import com.humanbooster.repository.UtilisateurRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,14 +20,50 @@ import java.util.Optional;
 public class BorneService {
 
     private final BorneRepository borneRepository;
+    private final LieuRepository lieuRepository;
+    private final AdresseRepository adresseRepository;
+    private final UtilisateurRepository utilisateurRepository;
 
     @Autowired
-    public BorneService(BorneRepository borneRepository) {
+    public BorneService(BorneRepository borneRepository, LieuRepository lieuRepository,
+                        AdresseRepository adresseRepository, UtilisateurRepository utilisateurRepository) {
         this.borneRepository = borneRepository;
+        this.lieuRepository = lieuRepository;
+        this.adresseRepository = adresseRepository;
+        this.utilisateurRepository = utilisateurRepository;
     }
 
     @Transactional
     public void saveBorne(Borne borne) {
+
+        Lieu lieu = borne.getLieu();
+        Adresse adresseLieu = lieu.getAdresse();
+        Utilisateur utilisateur = lieu.getUtilisateur();
+        Adresse adresseUtilisateur = utilisateur.getAdresse();
+
+        if (borne.getLieu().getId() == null) {
+            // Si le lieu n'existe pas, on le crée et on le save
+            if (adresseLieu != null && adresseLieu.getId() == null) {
+                adresseLieu = adresseRepository.save(adresseLieu);
+                borne.getLieu().setAdresse(adresseLieu);
+            }
+
+            // Si l'utilisateur du lieu n'existe pas, on le crée et on le save
+            if (utilisateur.getId() == null) {
+                if(adresseUtilisateur != null && adresseUtilisateur.getId() == null) {
+                    // Si l'adresse de l'utilisateur n'existe pas en BDD, on la save
+                    adresseUtilisateur = adresseRepository.save(adresseUtilisateur);
+                    borne.getLieu().getUtilisateur().setAdresse(adresseUtilisateur);
+                    utilisateurRepository.save(utilisateur);
+                }
+            }
+        }
+
+
+        if (lieu.getId() == null) {
+            lieu = lieuRepository.save(lieu);
+            borne.setLieu(lieu);
+        }
         borneRepository.save(borne);
     }
 
