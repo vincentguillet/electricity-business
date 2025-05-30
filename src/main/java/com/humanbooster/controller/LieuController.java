488cfec594f523c793ccaec085662b97c15f5987
Vincent Guillet <vincent.guillet@outlook.com>
2025-05-30 11:24:31 +0200

    refactor LieuController to simplify endpoint mappings

--- src/main/java/com/humanbooster/controller/LieuController.java
+++ src/main/java/com/humanbooster/controller/LieuController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/lieux")
 public class LieuController {
 
     private final LieuService lieuService;
@@ -18,24 +19,24 @@ public class LieuController {
         this.lieuService = lieuService;
     }
 
-    @GetMapping("/lieus")
-    public List<Lieu> getAllLieus() {
+    @GetMapping
+    public List<Lieu> getAllLieux() {
         return lieuService.getAllLieux();
     }
 
-    @GetMapping("/lieus/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Lieu> getLieuById(@PathVariable int id) {
         return lieuService.getLieuById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/lieus")
+    @PostMapping
     public void saveLieu(@RequestBody Lieu lieu) {
         lieuService.saveLieu(lieu);
     }
 
-    @PutMapping("/lieus/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Lieu> updateLieu(@PathVariable int id, @RequestBody Lieu lieu) {
         return lieuService.getLieuById(id)
                 .map(existingLieu -> lieuService.updateLieu(lieu)
@@ -44,7 +45,7 @@ public class LieuController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/lieus/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Lieu> deleteLieuById(@PathVariable int id) {
         return lieuService.deleteLieuById(id)
                 .map(lieu -> ResponseEntity.ok().body(lieu))
