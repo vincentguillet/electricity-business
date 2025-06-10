aeb61419e557748af238bee6c6e1827efa6e638b
Vincent Guillet <vincent.guillet@outlook.com>
2025-06-10 11:48:04 +0200

    feat: enhance LieuService to handle Adresse and Utilisateur saving logic

--- src/main/java/com/humanbooster/service/LieuService.java
+++ src/main/java/com/humanbooster/service/LieuService.java
@@ -1,7 +1,11 @@
 package com.humanbooster.service;
 
+import com.humanbooster.model.Adresse;
 import com.humanbooster.model.Lieu;
+import com.humanbooster.model.Utilisateur;
+import com.humanbooster.repository.AdresseRepository;
 import com.humanbooster.repository.LieuRepository;
+import com.humanbooster.repository.UtilisateurRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,14 +18,37 @@ import java.util.Optional;
 public class LieuService {
 
     private final LieuRepository lieuRepository;
+    private final AdresseRepository adresseRepository;
+    private final UtilisateurRepository utilisateurRepository;
 
     @Autowired
-    public LieuService(LieuRepository lieuRepository) {
+    public LieuService(LieuRepository lieuRepository, AdresseRepository adresseRepository, UtilisateurRepository utilisateurRepository) {
         this.lieuRepository = lieuRepository;
+        this.adresseRepository = adresseRepository;
+        this.utilisateurRepository = utilisateurRepository;
     }
 
     @Transactional
     public void saveLieu(Lieu lieu) {
+
+        Adresse adresseLieu = lieu.getAdresse();
+        Utilisateur utilisateur = lieu.getUtilisateur();
+        Adresse adresseUtilisateur = utilisateur.getAdresse();
+        
+        if (adresseLieu != null && adresseLieu.getId() == null) {
+            adresseLieu = adresseRepository.save(adresseLieu);
+            lieu.setAdresse(adresseLieu);
+        }
+
+        // Si l'utilisateur du lieu n'existe pas, on le crée et on le save
+        if (utilisateur.getId() == null) {
+            if(adresseUtilisateur != null && adresseUtilisateur.getId() == null) {
+                // Si l'adresse de l'utilisateur n'existe pas en BDD, on la save
+                adresseUtilisateur = adresseRepository.save(adresseUtilisateur);
+                lieu.getUtilisateur().setAdresse(adresseUtilisateur);
+                utilisateurRepository.save(utilisateur);
+            }
+        }
         lieuRepository.save(lieu);
     }
 
