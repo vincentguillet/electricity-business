5d5ceabbc8457b90df1e34b52f8a4194e4aef735
Vincent Guillet <vincent.guillet@outlook.com>
2025-07-12 12:23:04 +0200

    feat: add method to find Utilisateur by email in UtilisateurRepository

--- src/main/java/com/humanbooster/repository/UtilisateurRepository.java
+++ src/main/java/com/humanbooster/repository/UtilisateurRepository.java
@@ -3,7 +3,9 @@ package com.humanbooster.repository;
 import com.humanbooster.model.Utilisateur;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Integer> {
+    Optional<Utilisateur> findByEmail(String email);
 }
