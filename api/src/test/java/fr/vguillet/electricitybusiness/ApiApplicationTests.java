049ab09504ab42d91ce6ccc07649b80fb4753807
Vincent Guillet <vincent.guillet@outlook.com>
2025-12-06 18:05:32 +0100

    initialize project with Spring Boot and Maven setup

---
+++ api/src/test/java/fr/vguillet/electricitybusiness/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.vguillet.electricitybusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
