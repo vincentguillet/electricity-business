31a00e3a02d708693001f7755c73b200c735ace6
Vincent Guillet <vincent.guillet@outlook.com>
2025-12-09 11:38:24 +0100

    update ReservationRepository to use ReservationId as the identifier

--- api/src/main/java/fr/vguillet/electricitybusiness/repository/app/ReservationRepository.java
+++ api/src/main/java/fr/vguillet/electricitybusiness/repository/app/ReservationRepository.java
@@ -1,7 +1,8 @@
 package fr.vguillet.electricitybusiness.repository.app;
 
 import fr.vguillet.electricitybusiness.model.app.reservation.Reservation;
+import fr.vguillet.electricitybusiness.model.app.reservation.ReservationId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, ReservationId> {
 }
